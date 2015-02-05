644f8e4b29731e18aab072d7372aa4830b20b7a1
hectorballon <hector.ballon@gmail.com>
2015-02-05 07:09:25 -0500

    RC 0.1 - demo version for design

---
+++ Tanque/Prototype/forms/frmLogin.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Prototype.forms
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.llRegister = new System.Windows.Forms.LinkLabel();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.lbUserName = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.lbEnterprise = new System.Windows.Forms.Label();
+            this.lbYear = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(177, 108);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 13;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(258, 108);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 12;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // llRegister
+            // 
+            this.llRegister.AutoSize = true;
+            this.llRegister.Location = new System.Drawing.Point(21, 108);
+            this.llRegister.Name = "llRegister";
+            this.llRegister.Size = new System.Drawing.Size(46, 13);
+            this.llRegister.TabIndex = 11;
+            this.llRegister.TabStop = true;
+            this.llRegister.Text = "Register";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(21, 71);
+            this.tbPassword.MaxLength = 32;
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(171, 20);
+            this.tbPassword.TabIndex = 10;
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(18, 54);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(53, 13);
+            this.lbPassword.TabIndex = 9;
+            this.lbPassword.Text = "Password";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(22, 31);
+            this.tbUsername.MaxLength = 32;
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(170, 20);
+            this.tbUsername.TabIndex = 8;
+            // 
+            // lbUserName
+            // 
+            this.lbUserName.AutoSize = true;
+            this.lbUserName.Location = new System.Drawing.Point(19, 14);
+            this.lbUserName.Name = "lbUserName";
+            this.lbUserName.Size = new System.Drawing.Size(55, 13);
+            this.lbUserName.TabIndex = 7;
+            this.lbUserName.Text = "Username";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(212, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(212, 71);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 15;
+            // 
+            // lbEnterprise
+            // 
+            this.lbEnterprise.AutoSize = true;
+            this.lbEnterprise.Location = new System.Drawing.Point(209, 54);
+            this.lbEnterprise.Name = "lbEnterprise";
+            this.lbEnterprise.Size = new System.Drawing.Size(54, 13);
+            this.lbEnterprise.TabIndex = 16;
+            this.lbEnterprise.Text = "Enterprise";
+            // 
+            // lbYear
+            // 
+            this.lbYear.AutoSize = true;
+            this.lbYear.Location = new System.Drawing.Point(209, 14);
+            this.lbYear.Name = "lbYear";
+            this.lbYear.Size = new System.Drawing.Size(29, 13);
+            this.lbYear.TabIndex = 17;
+            this.lbYear.Text = "Year";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 144);
+            this.Controls.Add(this.lbYear);
+            this.Controls.Add(this.lbEnterprise);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.llRegister);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.lbUserName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.LinkLabel llRegister;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Label lbUserName;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label lbEnterprise;
+        private System.Windows.Forms.Label lbYear;
+    }
+}
---
+++ Tanque/Prototype/forms/frmLogin.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototype.forms
+{
+    public partial class frmLogin : Form
+    {
+        #region "Properties"
+
+        /// <summary>
+        /// Key for the crypto provider
+        /// </summary>
+        private static readonly byte[] _key = { 0xA1, 0xF1, 0xA6, 0xBB, 0xA2, 0x5A, 0x37, 0x6F, 0x81, 0x2E, 0x17, 0x41, 0x72, 0x2C, 0x43, 0x27 };
+        /// <summary>
+        /// Initialization vector for the crypto provider
+        /// </summary>
+        private static readonly byte[] _initVector = { 0xE1, 0xF1, 0xA6, 0xBB, 0xA9, 0x5B, 0x31, 0x2F, 0x81, 0x2E, 0x17, 0x4C, 0xA2, 0x81, 0x53, 0x61 };
+
+        public bool Authenticated { get; set; }
+
+        #endregion
+
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        // exit the program if they do not want to login
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // check that username and password are not empty
+        // lookup username in database and compare passwords - if ok continue
+        // if not ok, retry 2 more times then exit
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            Login();
+        }
+
+        private void Login()
+        {
+            Authenticated = false;
+
+            if (string.IsNullOrEmpty(tbUsername.Text))
+            {
+                //Focus box before showing a message
+                tbUsername.Focus();
+                MessageBox.Show("Enter your username", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //Focus again afterwards, sometimes people double click message boxes and select another control accidentally
+                tbUsername.Focus();
+                return;
+            }
+            else if (string.IsNullOrEmpty(tbPassword.Text))
+            {
+                tbPassword.Focus();
+                MessageBox.Show("Enter your password", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                tbPassword.Focus();
+                return;
+            }
+
+            if (UserAuthenticated(tbUsername.Text, tbPassword.Text))
+            {
+                Authenticated = true;
+                this.Close(); // close this form - do not exit the application
+            }
+            else
+            {
+                MessageBox.Show("Username or Password not recognised");
+            }
+        }
+
+        // Does the user exist?
+        // if so - is the password correct?
+        private bool UserAuthenticated(string user, string password)
+        {
+            try
+            {
+                int result = (int)usersTableAdapter.GetUserIDByUsernameAndPassword(user, password);
+                if (result > 0) return true;
+            }
+            catch (Exception) // FIXED: Added Exception catching  which defaults to Not Authenticated
+            {
+                return false;
+            }
+            return false;
+        }
+
+#if (DEBUG)
+        //Only compile this method for local debugging.
+        /// <summary>
+        /// Decrypt a string
+        /// </summary>
+        /// <param name="Value"></param>
+        /// <returns></returns>
+        private static string Decrypt(string Value)
+        {
+            SymmetricAlgorithm mCSP;
+            ICryptoTransform ct = null;
+            MemoryStream ms = null;
+            CryptoStream cs = null;
+            byte[] byt;
+            byte[] _result;
+
+            mCSP = new RijndaelManaged();
+
+            try
+            {
+                mCSP.Key = _key;
+                mCSP.IV = _initVector;
+                ct = mCSP.CreateDecryptor(mCSP.Key, mCSP.IV);
+
+
+                byt = Convert.FromBase64String(Value);
+
+                ms = new MemoryStream();
+                cs = new CryptoStream(ms, ct, CryptoStreamMode.Write);
+                cs.Write(byt, 0, byt.Length);
+                cs.FlushFinalBlock();
+
+                cs.Close();
+                _result = ms.ToArray();
+            }
+            catch
+            {
+                _result = null;
+            }
+            finally
+            {
+                if (ct != null)
+                    ct.Dispose();
+                if (ms != null)
+                    ms.Dispose();
+                if (cs != null)
+                    cs.Dispose();
+            }
+
+            return ASCIIEncoding.UTF8.GetString(_result);
+        }
+#endif
+
+        /// <summary>
+        /// Encrypt a string
+        /// </summary>
+        /// <param name="Password"></param>
+        /// <returns></returns>
+        private static string Encrypt(string Password)
+        {
+            if (string.IsNullOrEmpty(Password))
+                return string.Empty;
+
+            byte[] Value = Encoding.UTF8.GetBytes(Password);
+            SymmetricAlgorithm mCSP = new RijndaelManaged();
+            mCSP.Key = _key;
+            mCSP.IV = _initVector;
+            using (ICryptoTransform ct = mCSP.CreateEncryptor(mCSP.Key, mCSP.IV))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, ct, CryptoStreamMode.Write))
+                    {
+                        cs.Write(Value, 0, Value.Length);
+                        cs.FlushFinalBlock();
+                        cs.Close();
+                        return Convert.ToBase64String(ms.ToArray());
+                    }
+                }
+            }
+        }
+    }
+}
